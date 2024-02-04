652bfe1076dd4ecff0cb03551894d681ee29ee62
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2024-02-04 22:58:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import streamlit as st
 zip_file_path = "cs prac.zip"
 
 # Create a download button
-st.download_button(label='clear memory(refresh)', data=zip_file_path, file_name='your_zip_file.zip', mime='application/zip')
+st.download_button(label='clear memory(refresh)', data=zip_file_path, file_name='log.zip', mime='application/zip')
