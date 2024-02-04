51fcbb7ba4ee48dd7e0c848b40fc5ba46b090d0b
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2024-02-04 23:01:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import streamlit as st
+import base64
 
 # Path to your zip file
 zip_file_path = "cs prac.zip"
 
+# Function to read the contents of the zip file
+def read_zip_file(zip_file_path):
+    with open(zip_file_path, "rb") as f:
+        zip_data = f.read()
+    return zip_data
+
+# Read the contents of the zip file
+zip_data = read_zip_file(zip_file_path)
+
 # Create a download button
-st.download_button(label='clear memory(refresh)', data=zip_file_path, file_name='log.zip', mime='application/zip')
+b64 = base64.b64encode(zip_data).decode()
+href = f'<a href="data:application/zip;base64,{b64}" download="your_zip_file.zip">Download Zip File</a>'
+st.markdown(href, unsafe_allow_html=True)
